314a1f2cf26822ca893afb02cd52075ac90f24b0
graysonpetter <graysonpetter@gmail.com>
2020-08-27 15:32:08 -0400

    added whole sky lambert projection

--- convergence_map.py
+++ convergence_map.py
@@ -8,7 +8,7 @@ nsides = 2048
 # take in healpix map which defaults to using the UNSEEN value to denote masked pixels and return
 # a masked map with NaNs instead
 def set_unseen_to_nan(map):
-    map[np.where(map == hp.UNSEEN)[0]] = np.nan
+    map[np.where(np.logical_or(map == hp.UNSEEN, np.logical_and(map < -1e30, map > -1e31)))] = np.nan
     return map
 
 
@@ -29,56 +29,20 @@ def set_unseen_to_mask(map):
 def zero_modes(almarr, l_cutoff):
     lmax = hp.Alm.getlmax(len(almarr))
     l, m = hp.Alm.getlm(lmax=lmax)
-    idxs = np.where(l < l_cutoff)[0]
+    idxs = np.where(l < l_cutoff)
     almarr[idxs] = 0.0j
     return almarr
 
 
-# read in a klm fits lensing convergence map, zero l modes desired, and remove the mean field if desired
-def read_planck_map(remove_meanfield):
+# read in a klm fits lensing convergence map, zero l modes desired, write out map
+def klm_2_map(klmname, mapname):
     # read in planck alm convergence data
-    planck_lensing_alm = hp.read_alm('dat_klm.fits')
+    planck_lensing_alm = hp.read_alm(klmname)
     filtered_alm = zero_modes(planck_lensing_alm, 100)
     # generate map from alm data
     planck_lensing_map = hp.sphtfunc.alm2map(filtered_alm, nsides, lmax=4096)
-    
-    if remove_meanfield:
-        #print('Removing mean field: <kappa> = ')
-        #print(np.mean(planck_lensing_map))
-        # read in mask
-        mask = hp.read_map('mask.fits')
-        # apply mask, fill in masked values with UNSEEN
-        masked_map = hp.ma(planck_lensing_map)
-        masked_map.mask = np.logical_not(mask)
-        masked_map = masked_map.filled()
-        # convert UNSEEN mask to NaN for mean subtraction
-        masked_map = set_unseen_to_nan(masked_map)
-        meanfield = np.nanmean(masked_map)
-        masked_map = masked_map - meanfield
-        masked_map = set_nan_to_unseen(masked_map)
-    
     # write out lensing convergence map
-    hp.write_map('planck_map.fits', planck_lensing_map, overwrite=True)
-
-
-# mask map and remove the mean field if desired
-def mask_map(map, mask, remove_meanfield, outmap):
-    # read in map and mask
-    importmap = hp.read_map(map)
-    importmask = hp.read_map(mask).astype(np.bool)
-    # set mask, invert
-    masked_map = hp.ma(importmap)
-    masked_map.mask = np.logical_not(importmask)
-    masked_map = masked_map.filled()
-    
-    # subtract mean field
-    if remove_meanfield:
-        masked_map = set_unseen_to_nan(masked_map)
-        meanfield = np.nanmean(masked_map)
-        masked_map = masked_map - meanfield
-        masked_map = set_nan_to_unseen(masked_map)
-
-    hp.write_map(outmap, masked_map, overwrite=True)
+    hp.write_map(mapname, planck_lensing_map, overwrite=True)
 
 
 # smooth map with gaussian of fwhm = width arcminutes
@@ -88,6 +52,19 @@ def smooth_map(mapname, width, outname):
     smoothed_map = hp.sphtfunc.smoothing(map, fwhm=fwhm)
 
     hp.write_map(outname, smoothed_map, overwrite=True)
+
+
+# mask map and remove the mean field if desired
+def mask_map(map, mask, outmap):
+    # read in map and mask
+    importmap = hp.read_map(map)
+    importmask = hp.read_map(mask).astype(np.bool)
+    # set mask, invert
+    masked_map = hp.ma(importmap)
+    masked_map.mask = np.logical_not(importmask)
+    masked_map = masked_map.filled()
+
+    hp.write_map(outmap, masked_map, overwrite=True)
 
 
 # can change coordinates from ecliptic to equatorial for example, no longer needed
@@ -116,4 +93,12 @@ def change_coord(mapname, coord, outname):
     new_pix = hp.ang2pix(nside, *new_ang)
     
     hp.write_map(outname, m[..., new_pix], overwrite=True)
+
+
+def lamb_map(mapname, reso):
+    importmap = hp.read_map(mapname, dtype=np.single)
+    lambert = (hp.azeqview(importmap, xsize=12000, ysize=12000, reso=reso, lamb=True, return_projected_map=True)).filled()
+    lambert[np.where(lambert == -np.inf)] = hp.UNSEEN
+
+    lambert.astype(np.single).dump('lambert_planck.npy')
 
